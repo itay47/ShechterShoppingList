5387d6fe9fddec8d3075dcbf5b7074a100282034
Itay Shechter <ItayLcars@hotmail.com>
2019-10-27 16:24:34 +0200

    Removed Privacy

--- ShechterShoppingList/Controllers/HomeController.cs
+++ ShechterShoppingList/Controllers/HomeController.cs
@@ -15,10 +15,6 @@ namespace ShechterShoppingList.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
