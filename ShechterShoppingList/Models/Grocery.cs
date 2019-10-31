901305499287d32e26afb36f7dcfb5d0518ad587
Itay Shechter <ItayLcars@hotmail.com>
2019-10-31 17:49:20 +0200

    Added Grocery class & AWS DynamoDB nuget package

---
+++ ShechterShoppingList/Models/Grocery.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace ShechterShoppingList.Models
+{
+    [DynamoDBTable("ShechterShoppingList")]
+    public class Grocery
+    {
+        [DynamoDBHashKey]
+        public Guid Id { get; set; }
+        public string GroceyName { get; set; }
+        public int Ammount { get; set; }
+        public string Measure { get; set; }
+
+        public enum UnitOfMeasure
+        {
+            Gr,
+            Kg,
+            Unit,
+            Box,
+        }
+    }
+}
