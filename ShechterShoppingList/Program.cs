750d04ee4809e120d7433f473e49ae321d741c8e
Itay Shechter <ItayLcars@hotmail.com>
2019-11-10 10:37:58 +0200

    changed working ports: custom ports <http://+5000,https://+5001>

--- ShechterShoppingList/Program.cs
+++ ShechterShoppingList/Program.cs
@@ -19,6 +19,7 @@ namespace ShechterShoppingList
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://+:5000","https://+:5001");
     }
 }
