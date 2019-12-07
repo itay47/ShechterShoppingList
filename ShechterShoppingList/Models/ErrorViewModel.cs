c4cee362b5ee6c9308727e459692acc8daddddfb
itay47 <itaylcars@hotmail.com>
2019-12-07 19:16:07 +0200

    * Upgraded ENV to  ASPnet core 3.x * updated Dockerfile for .net core 3.1 * added ErrorPage for loading page errors.

--- ShechterShoppingList/Models/ErrorViewModel.cs
+++ ShechterShoppingList/Models/ErrorViewModel.cs
@@ -7,5 +7,6 @@ namespace ShechterShoppingList.Models
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string ErrorMessage {get;set;}
     }
 }
