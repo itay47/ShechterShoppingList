e5612293f46f746088ad38caded473a30b50530d
itay47 <Itaylcars@hotmail.com>
2019-12-20 15:23:33 +0200

    Update ErrorViewModel.cs
    implements AWS errors

--- ShechterShoppingList/Models/ErrorViewModel.cs
+++ ShechterShoppingList/Models/ErrorViewModel.cs
@@ -7,5 +7,9 @@ namespace ShechterShoppingList.Models
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string ErrorMessage {get;set;}
+        public string AWS_SECRET_ACCESS_KEY {get;set;}
+        public string AWS_ACCESS_KEY_ID {get;set;}
+        public string AWS_REGION {get;set;}
     }
 }
