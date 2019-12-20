94b31e94ddf3d02720d67954938d2f104be40cf2
itay47 <Itaylcars@hotmail.com>
2019-12-20 15:21:45 +0200

    Update HomeController.cs
    fixed error handling view if cannot connect to AWS DB

--- ShechterShoppingList/Controllers/HomeController.cs
+++ ShechterShoppingList/Controllers/HomeController.cs
@@ -1,18 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Mime;
+using System.Threading;
 using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Runtime;
 using Microsoft.AspNetCore.Mvc;
 using ShechterShoppingList.Models;
 using ShechterShoppingList.ViewModels;
-
-using Amazon.DynamoDBv2;
-using Amazon.DynamoDBv2.DocumentModel;
-using Amazon.DynamoDBv2.DataModel;
-using Amazon.Runtime;
-using Amazon.DynamoDBv2.Model;
-using System.Threading;
 
 namespace ShechterShoppingList.Controllers
 {
@@ -22,53 +22,78 @@ namespace ShechterShoppingList.Controllers
         //private const string tableName = "ShechterShoppingList";
         //public DynamoDBContext DBContext { get; set; }
 
-        public IActionResult Index(Grocery grocery)
+        public async Task<IActionResult> Index (Grocery grocery)
         {
-            if (string.IsNullOrEmpty(grocery.GroceyName))
-            {
-                var groceries = DynamoDbCRUDOperations.GetDataAsync().Result;
-                GroceryViewModel viewModel = new GroceryViewModel { Groceries = groceries };
-
-                return View("Index", viewModel);
-            }
-            return View();
-            
-        }
-
-        [Route("/Home/Edit",Name ="Edit")]
-        public IActionResult Edit(string Id)
-        {
-            var guid = Guid.Parse(Id);
-            Grocery grocery = DynamoDbCRUDOperations.GetItemsById(guid).Result;
-            return View(grocery);
-        }
-
-        public async Task<IActionResult> UpdateDataAsync(string gId, string gName, int gAmmount, Grocery gModel)
-        {
-            if (string.IsNullOrEmpty(gId) || string.IsNullOrEmpty(gName) || string.IsNullOrEmpty(gModel.Measure))
-            {
-                return BadRequest();
-            }
-
-            var guid = Guid.Parse(gId);
-            var lst = new List<Grocery>();
             try
             {
-                Grocery grocery = DynamoDbCRUDOperations.GetItemsById(guid).Result;
-                lst.Add(grocery);
+                if (string.IsNullOrEmpty (grocery.GroceyName))
+                {
+                    var results = await DynamoDbCRUDOperations.GetDataAsync ();
+                    var groceries = results;
+                    GroceryViewModel viewModel = new GroceryViewModel { Groceries = groceries };
+
+                    return View ("Index", viewModel);
+                }
+                return View ();
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction ("Error", "Home", new { message = e.Message });
+            }
+        }
+
+        [Route ("/Home/Error", Name = "Error")]
+        public async Task<IActionResult> ErrorPage (string message)
+        {
+            var awsregion = string.IsNullOrEmpty(EnvironmentVariableAWSRegion.ENVIRONMENT_VARIABLE_REGION.SingleOrDefault ().ToString ())? "null": EnvironmentVariableAWSRegion.ENVIRONMENT_VARIABLE_REGION.SingleOrDefault ().ToString ();
+            var awsid = string.IsNullOrEmpty(EnvironmentVariablesAWSCredentials.ENVIRONMENT_VARIABLE_ACCESSKEY.SingleOrDefault().ToString())? "null" : EnvironmentVariablesAWSCredentials.ENVIRONMENT_VARIABLE_ACCESSKEY.SingleOrDefault().ToString();
+            var awskey = string.IsNullOrEmpty(EnvironmentVariablesAWSCredentials.ENVIRONMENT_VARIABLE_SECRETKEY.SingleOrDefault().ToString())? "null" : EnvironmentVariablesAWSCredentials.ENVIRONMENT_VARIABLE_SECRETKEY.SingleOrDefault().ToString();;
+
+            ErrorViewModel evm = new ErrorViewModel ();
+            await Task.Delay (100);
+            evm.ErrorMessage = message;
+            evm.AWS_ACCESS_KEY_ID = awsid;
+            evm.AWS_SECRET_ACCESS_KEY = awskey;
+            evm.AWS_REGION = awsregion;
+
+            return View ("ErrorPage", evm);
+        }
+
+        [Route ("/Home/Edit", Name = "Edit")]
+        public async Task<IActionResult> Edit (string Id)
+        {
+            var guid = Guid.Parse (Id);
+            var results = await DynamoDbCRUDOperations.GetItemsById (guid);
+            Grocery grocery = results;
+            return View (grocery);
+        }
+
+        public async Task<IActionResult> UpdateDataAsync (string gId, string gName, int gAmmount, Grocery gModel)
+        {
+            if (string.IsNullOrEmpty (gId) || string.IsNullOrEmpty (gName) || string.IsNullOrEmpty (gModel.Measure))
+            {
+                return BadRequest ();
+            }
+
+            var guid = Guid.Parse (gId);
+            var lst = new List<Grocery> ();
+            try
+            {
+                Grocery grocery = DynamoDbCRUDOperations.GetItemsById (guid).Result;
+                lst.Add (grocery);
 
                 Grocery updatedData = new Grocery
                 {
                     Ammount = gAmmount,
-                    DateModified = DateTime.Parse(DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString()),
+                    DateModified = DateTime.Parse (DateTime.Now.ToShortDateString () + " " + DateTime.Now.ToShortTimeString ()),
                     GroceyName = gName,
                     Id = guid,
                     Measure = gModel.Measure,
                     Done = grocery.Done,
                 };
 
-                await DynamoDbCRUDOperations.UpdateItemAsync(lst, updatedData);
-                return RedirectToAction("Index","Home",null);
+                await DynamoDbCRUDOperations.UpdateItemAsync (lst, updatedData);
+                return RedirectToAction ("Index", "Home", null);
             }
             catch (Exception ex)
             {
@@ -77,28 +102,28 @@ namespace ShechterShoppingList.Controllers
             }
         }
 
-        public async Task<IActionResult> AddNewGroceryAsync(string gName, int gAmmount, GroceryViewModel gModel)
+        public async Task<IActionResult> AddNewGroceryAsync (string gName, int gAmmount, GroceryViewModel gModel)
         {
-            if ( string.IsNullOrEmpty(gName) || string.IsNullOrEmpty(gModel.NewGrocery.Measure))
+            if (string.IsNullOrEmpty (gName) || string.IsNullOrEmpty (gModel.NewGrocery.Measure))
             {
-                return BadRequest();
+                return BadRequest ();
             }
 
-            var guid = Guid.NewGuid();
+            var guid = Guid.NewGuid ();
             try
             {
                 Grocery newGrocery = new Grocery
                 {
                     Ammount = gAmmount,
-                    DateModified = DateTime.Parse(DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString()),
+                    DateModified = DateTime.Parse (DateTime.Now.ToShortDateString () + " " + DateTime.Now.ToShortTimeString ()),
                     GroceyName = gName,
                     Id = guid,
                     Measure = gModel.NewGrocery.Measure,
                     Done = false
                 };
 
-                await DynamoDbCRUDOperations.AddItemAsync(newGrocery);
-                return RedirectToAction("Index", "Home", null);
+                await DynamoDbCRUDOperations.AddItemAsync (newGrocery);
+                return RedirectToAction ("Index", "Home", null);
             }
             catch (Exception ex)
             {
@@ -107,26 +132,26 @@ namespace ShechterShoppingList.Controllers
             }
         }
 
-        public async Task<IActionResult> DeleteItemAsync(Grocery gId)
+        public async Task<IActionResult> DeleteItemAsync (Grocery gId)
         {
             if (gId.Id == null)
             {
-                return BadRequest();
+                return BadRequest ();
             }
 
             var guid = gId.Id;
-            
+
             try
             {
-                Grocery grocery = DynamoDbCRUDOperations.GetItemsById(guid).Result;
+                Grocery grocery = DynamoDbCRUDOperations.GetItemsById (guid).Result;
                 if (grocery != null)
                 {
-                    await DynamoDbCRUDOperations.DeleteItemAsync(guid);
+                    await DynamoDbCRUDOperations.DeleteItemAsync (guid);
 
-                    return RedirectToAction("Index", "Home", null);
+                    return RedirectToAction ("Index", "Home", null);
                 }
                 else
-                    return BadRequest();
+                    return BadRequest ();
             }
             catch (Exception ex)
             {
@@ -135,53 +160,50 @@ namespace ShechterShoppingList.Controllers
             }
         }
 
-
-        public async Task<IActionResult> ToggleDoneAsync(Grocery gId)
+        public async Task<IActionResult> ToggleDoneAsync (Grocery gId)
         {
             if (gId.Id == null)
             {
-                return BadRequest();
+                return BadRequest ();
             }
 
             var guid = gId.Id;
-            var lst = new List<Grocery>();
+            var lst = new List<Grocery> ();
             try
             {
-                Grocery grocery = DynamoDbCRUDOperations.GetItemsById(guid).Result;
-                lst.Add(grocery);
+                Grocery grocery = DynamoDbCRUDOperations.GetItemsById (guid).Result;
+                lst.Add (grocery);
 
                 Grocery updatedData = new Grocery
                 {
                     Ammount = grocery.Ammount,
-                    DateModified = DateTime.Parse(DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString()),
+                    DateModified = DateTime.Parse (DateTime.Now.ToShortDateString () + " " + DateTime.Now.ToShortTimeString ()),
                     GroceyName = grocery.GroceyName,
                     Id = guid,
                     Measure = grocery.Measure,
                     Done = !grocery.Done,
                 };
 
-                await DynamoDbCRUDOperations.UpdateItemAsync(lst, updatedData);
-                return RedirectToAction("Index", "Home", null);
+                await DynamoDbCRUDOperations.UpdateItemAsync (lst, updatedData);
+                return RedirectToAction ("Index", "Home", null);
             }
             catch (Exception ex)
             {
-
                 throw ex;
             }
         }
 
-        public HomeController(IAmazonDynamoDB amazonDBService)
+        public HomeController (IAmazonDynamoDB amazonDBService)
         {
-            DynamoDbCRUDOperations operations = new DynamoDbCRUDOperations(amazonDBService);
+            DynamoDbCRUDOperations operations = new DynamoDbCRUDOperations (amazonDBService);
             //var groceries = DynamoDbCRUDOperations.GetDataAsync().Result;
 
         }
-        
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [ResponseCache (Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error ()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View (new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
