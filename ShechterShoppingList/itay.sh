0a8d2bdc4e8368e336ee821d7cf8bee83327856f
itay47 <Itaylcars@hotmail.com>
2019-12-20 15:17:25 +0200

    Added itay.sh Bash script
    this script runs configures AWS profile from ENV passed to docker and then runs the dotnet application

---
+++ ShechterShoppingList/itay.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "ENV:id="$AWS_ACCESS_KEY_ID
+echo "ENV:secret="$AWS_SECRET_ACCESS_KEY
+
+# call aws to create a profile
+echo "creating aws profile file"
+/usr/local/bin/aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+/usr/local/bin/aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+
+#call dotnet mydll.dll to run the application
+echo "calling dotnet application"
+cd /app/
+dotnet ShechterShoppingList.dll
